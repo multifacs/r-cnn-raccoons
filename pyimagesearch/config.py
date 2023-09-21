b71ec9f4dda9588d603fe9c2510835f516737b93
multifacs <kiwi0tails@gmail.com>
2023-09-21 14:19:05 +0300

    config

--- pyimagesearch/config.py
+++ pyimagesearch/config.py
@@ -17,3 +17,17 @@ NEGATIVE_PATH = os.path.sep.join([BASE_PATH, "no_raccoon"])
 # search for (1) gathering training data and (2) performing inference
 MAX_PROPOSALS = 2000
 MAX_PROPOSALS_INFER = 200
+
+# define the maximum number of positive and negative images to be
+# generated from each image
+MAX_POSITIVE = 30
+MAX_NEGATIVE = 10
+
+# initialize the input dimensions to the network
+INPUT_DIMS = (224, 224)
+# define the path to the output model and label binarizer
+MODEL_PATH = "raccoon_detector.h5"
+ENCODER_PATH = "label_encoder.pickle"
+# define the minimum probability required for a positive prediction
+# (used to filter out false-positive predictions)
+MIN_PROBA = 0.99
